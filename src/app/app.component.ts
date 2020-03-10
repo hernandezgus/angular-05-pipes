0a943ae789728866a77f334f59c4dda4fed7bbc5
hernandezgus <hernandez.gustavo@outlook.com>
2020-03-10 20:29:09 -0300

    Adding common pipes examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  nombre = 'Capitán América';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  PI: number = Math.PI;
+  porcentaje = 0.234;
+  salario = 1234.5;
+
+  valorPromesa = new Promise<string>( (resolve) => {
+    setTimeout(() => {
+      resolve('llegó la data');
+    }, 2000);
+  });
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: '500',
+    direccion: {
+      calle: 'primera 234',
+      casa: 20
+    }
+  };
 }
